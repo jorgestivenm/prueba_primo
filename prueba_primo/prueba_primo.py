a5c469296794d63c34b73cd4da87e08254b0a4b0
jorgestivenm <jorgestivenm@gmail.com>
2018-03-23 12:59:16 -0500

    Update documentation with more details

--- prueba_primo/prueba_primo.py
+++ prueba_primo/prueba_primo.py
@@ -4,29 +4,29 @@
 
 
 def es_primo(x):
-	if x < 2:
-		return False
-	elif x == 2:
-		return True
-	else:
-		for n in range(2, x):
-			if x % n == 0:
-				return False
-			elif(n == x - 1):
-				return True
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    else:
+        for n in range(2, x):
+            if x % n == 0:
+                return False
+            elif(n == x - 1):
+                return True
 
 
 def comprobar(numero):
-	while True:
-		try:
-			# numero = int(raw_input("inserta un numero (0) salir >> "))
-			if numero == 0:
-				break
-			if es_primo(numero):
-				print ("\nEl numero %s es primo" % numero)
-				return numero
-			else:
-				print ("\nEl numero %s NO es primo" % numero)
-				break
-		# except:
-			# print ("\nEl numero tiene que ser entero")
+    while True:
+        try:
+            # numero = int(raw_input("inserta un numero (0) salir >> "))
+            if numero == 0:
+                break
+            if es_primo(numero):
+                print("\n El numero %s es primo" % numero)
+                return numero
+            else:
+                print("\n El numero %s NO es primo" % numero)
+                break
+        except False:
+            print("error/exception")
